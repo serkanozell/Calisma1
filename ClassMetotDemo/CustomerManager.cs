692c190530a901dced394333db2a36cf2bbfc9ce
serkan özel <serkanozel399@gmail.com>
2021-05-07 00:47:16 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+     class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine(customer.Name + " Kişisi eklendi.");
+        }
+
+        public void List(Customer customer1, Customer customer2)
+        {
+            Customer[] customerList = new Customer[]
+            {
+                customer1,
+                customer2
+            };
+
+            foreach (var customer in customerList)
+            {
+                Console.WriteLine("Adı : " + customer.Name);
+                Console.WriteLine("Soyadı : " + customer.Surname);
+                Console.WriteLine("Şehir : " + customer.City);
+            }
+        }
+
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine(" Kişisi silindi" + customer.Name);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+
+            customer1.Id = 1;
+            customer1.Name = "Serkan";
+            customer1.Surname = "ÖZEL";
+            customer1.City = "İstanbul";
+
+
+            Customer customer2 = new Customer();
+
+            customer2.Id = 2;
+            customer2.Name = "Ahmet";
+            customer2.Surname = "Ali";
+            customer2.City = "Ankara";
+
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            customerManager.Add(customer2);
+
+
+            customerManager.List(customer1, customer2);
+
+
+            customerManager.Delete(customer1);
+            customerManager.Delete(customer2);
+        }
+    }
+}
